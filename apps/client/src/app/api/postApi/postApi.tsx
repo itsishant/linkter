f70651e27d95fe2817dc01d7c368c9869f53b511
itsishant <modakmania@gmail.com>
2025-10-22 00:42:40 +0530

    add signin

--- apps/client/src/app/api/postApi/postApi.tsx
+++ apps/client/src/app/api/postApi/postApi.tsx
@@ -28,9 +28,22 @@ const verifyOtp = async (data: { email: string; otp: string; password: string; }
     return await response.json();
 };
 
+const signIn = async (data: {email: string, password: string}) => {
+    const response = await fetch(`${baseUrl}/signin`, {
+         method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    })
+
+      if (!response.ok) {
+        const message = `Error: ${response.status}`;
+        throw new Error(message);
+    }
+    return await response.json();
+}
 
 export {
     createUser,
-    verifyOtp
+    verifyOtp,
+    signIn
 }
-
